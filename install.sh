54ac3d49c0dc1dd3a871b5f73d7f2efbe5db2e8e
Axel De Santiago <landsome@tutanota.com>
2020-12-11 15:10:28 -0600

    fc-cache -f -v

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ ask() {
 if ask "Install missing directories for Stow?" Y; then
 	echo "Installing directories"
 	mkdir -pv ~/.config/mpv ~/.config/i3 ~/.config/ranger ~/.vim ~/.config/mpv ~/.local/share/fonts
+  fc-cache -f -v
 	fi
 
 if ask "Install Saucecode Pro Font?" Y; then
